c8fe0246ecfd6b96b77e8b432a623013372d38b6
John Cris <johncrismanabo9@gmail.com>
2025-06-17 14:38:28 +0800

    Replace home route with a redirect to dashboard and comment out the original welcome route.

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
+// Route::get('/', function () {
+//     return Inertia::render('welcome');
+// })->name('home');
+
+Route::redirect('/', 'dashboard')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
