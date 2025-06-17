4f4a3dcf0440be5d6cf740062e3c04e684223213
John Cris <johncrismanabo9@gmail.com>
2025-06-17 16:06:39 +0800

    Add Users page component for user management
    - Created a new `Users` component to display a list of users with pagination and a button to add new users.
    - Integrated `DataTable` for enhanced data presentation and interaction.
    - Set up breadcrumbs for navigation within the user management section.
    - The component is designed to work within the `AppLayout` for consistent UI structure.

---
+++ resources/js/pages/user/index.tsx
@@ -0,0 +1,58 @@
+import { Head } from "@inertiajs/react";
+import { BreadcrumbItem, PageProps } from "@/types";
+import { Button } from "@/components/ui/button";
+import { columns } from "@/components/data-table/columns";
+import AppLayout from "@/layouts/app-layout";
+import { Plus } from "lucide-react";
+import { DataTable } from "@/components/ui/data-table";
+
+export default function Users({ users }: PageProps) {
+
+    const breadcrumbs: BreadcrumbItem[] = [
+        {
+            title: 'User',
+            href: '/user',
+        },
+    ];
+
+    return (
+        <AppLayout breadcrumbs={breadcrumbs}>
+            <Head title="Users" />
+            <div className="container mx-auto space-y-6 px-5 py-6">
+                <div className="flex items-center justify-between">
+                    <h6 className="text-2x font-bold">User</h6>
+
+                    <Button  >
+                        <Plus />
+                        Add New
+                    </Button>
+
+                </div>
+                <DataTable columns={columns} data={users.data} meta={users.meta} />
+            </div>
+
+            {/* <Dialog open={showDeleteDialog} onOpenChange={setShowDeleteDialog}>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>Delete Selected Users</DialogTitle>
+                        <DialogDescription>
+                            Are you sure you want to delete {selectedIds.length} users? This
+                            action cannot be undone.
+                        </DialogDescription>
+                    </DialogHeader>
+                    <DialogFooter>
+                        <Button
+                            variant="outline"
+                            onClick={() => setShowDeleteDialog(false)}
+                        >
+                            Cancel
+                        </Button>
+                        <Button variant="destructive" onClick={handleBulkDelete}>
+                            Delete Users
+                        </Button>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog> */}
+        </AppLayout>
+    );
+}
