5a8b84bd8196fe32b3e253e0f87cdd3a34d7a27b
John Cris <johncrismanabo9@gmail.com>
2025-06-19 09:24:23 +0800

    Refactor AppLogo component to use Avatar for branding
    - Replaced AppLogoIcon with Avatar component displaying the new logo image.
    - Updated styling for improved layout and visual consistency.
    - Adjusted text properties for better readability of the application name.

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,15 +1,17 @@
-import AppLogoIcon from './app-logo-icon';
-
+import { Avatar, AvatarImage } from '@/components/ui/avatar';
+import logo from '../../image/logo.png';
 export default function AppLogo() {
 
     const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
     return (
         <>
-            <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+            <div className="bg-sidebar-primary   flex aspect-square size-8 items-center justify-center rounded-md">
+                <Avatar className="h-10 w-10  ">
+                    <AvatarImage src={logo} alt='Logo' />
+                </Avatar>
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-tight font-semibold">{appName}</span>
+                <span className="mb-0.5 truncate leading-none font-semibold">{appName}</span>
             </div>
         </>
     );
