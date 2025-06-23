039d2ab34cc3d6fb887da3d7796957b64022a916
John Cris <johncrismanabo9@gmail.com>
2025-06-23 11:02:45 +0800

    Add Office routes to web.php
    - Registered resource routes for the OfficeController to handle CRUD operations for offices, enhancing the application's routing structure.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\OfficeController;
 use App\Http\Controllers\PermissionController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\Settings\ProfileController;
@@ -17,7 +18,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::post('/users/{users}/avatar', [ProfileController::class, 'updateAvatar'])->name('user.update_avatar');
     Route::resource('roles', RoleController::class)->middleware('can:view roles');
     Route::resource('permissions', PermissionController::class)->middleware('can:view permissions');
-
+    Route::resource('offices', OfficeController::class);
 });
 
 require __DIR__.'/settings.php';
