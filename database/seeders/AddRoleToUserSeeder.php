239195ca1d44149e93998260b069d3da101529a7
unknown <masochist608@gmail.com>
2025-06-24 10:23:18 +0800

    Changed Seeders For Convinience

--- database/seeders/AddRoleToUserSeeder.php
+++ database/seeders/AddRoleToUserSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class AddRoleToUserSeeder extends Seeder
 {
@@ -12,9 +14,13 @@ class AddRoleToUserSeeder extends Seeder
      */
     public function run(): void
     {
-        $user = User::find(2);
+        $user = User::find(1); // change number 1 to ID of account you want to give permissions
+        $role = Role::where('name', 'Super Admin')->first();
+        $user->assignRole($role);
+        $role->syncPermissions(Permission::all());
 
-        $user->assignRole('Super Admin');
+        $user->getAllPermissions()->pluck('name');
 
+        //should output Illuminate\Support\Collection {#... all: ["view divisions", "create divisions", "edit divisions", ...] }
     }
 }
