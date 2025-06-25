207abef0e832d809fd21b1fe6ff6a4a6ce854d17
John Cris <johncrismanabo9@gmail.com>
2025-06-25 09:19:51 +0800

    Enhance RowActions component with role-based access and success/error notifications
    - Integrated role-based access control for edit and delete actions in the RowActions component, ensuring only authorized users can perform these actions.
    - Added success and error notifications using the toast library to provide user feedback upon successful deletion or error occurrence.

--- resources/js/pages/offices/columns.tsx
+++ resources/js/pages/offices/columns.tsx
@@ -2,6 +2,8 @@ import { ColumnDef } from "@tanstack/react-table";
 import { DataTableColumnHeader } from "@/components/ui/data-table-column-header";
 import { RowActions } from "@/components/DataTable/RowActions";
 import { router } from "@inertiajs/react";
+import { toast } from "sonner";
+import { can } from "@/lib/can";
 
 export type Office = {
     id: number;
@@ -21,20 +23,31 @@ export const columns: ColumnDef<Office>[] = [
                 <RowActions
                     item={office}
                     actions={[
-                        {
+                        ...(can('edit offices') ? [{
                             label: "Edit",
                             href: route("offices.edit", office),
-                        },
-                        {
+                        }] : []),
+
+                        ...(can('delete offices') ? [{
                             label: "Delete",
                             requiresConfirmation: true,
                             onClick: () => {
                                 router.delete(route(`offices.destroy`, office), {
                                     preserveScroll: true,
+                                    onSuccess: () => {
+                                        toast.success('Success', {
+                                            description: `Office deleted successfully`,
+                                        });
+                                    },
+                                    onError: (errors) => {
+                                        console.error(errors);
+                                    },
 
                                 });
                             },
-                        },
+                        },] : []),
+
+
                     ]}
                 />
             );
